7e61f9a745679818882d0680f8f0d8003ac27328
SupakornTha <36919622+SupakornTha@users.noreply.github.com>
2018-12-17 09:35:49 +0700

    gmange appoint

--- app/src/main/java/com/example/user/test2/AppoinmentActivity.java
+++ app/src/main/java/com/example/user/test2/AppoinmentActivity.java
@@ -1,49 +1,77 @@
 package com.example.user.test2;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.RectF;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.CheckBox;
 
-import com.alamkanak.weekview.MonthLoader;
-import com.alamkanak.weekview.WeekView;
-import com.alamkanak.weekview.WeekViewEvent;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 public class AppoinmentActivity extends Activity {
 
-    private WeekView mWeekView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoinmrnts);
 
-        mWeekView = (WeekView) findViewById(R.id.weekView);
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
+        CheckBox checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
+        CheckBox checkBox6 = (CheckBox) findViewById(R.id.checkBox6);
 
-        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
-            @Override
-            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+        boolean checked = PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean("checkBox", false);
 
-            }
-        });
+        checkBox.setChecked(checked);
+        checkBox2.setChecked(checked);
+        checkBox3.setChecked(checked);
+        checkBox4.setChecked(checked);
+        checkBox5.setChecked(checked);
+        checkBox6.setChecked(checked);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-        mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
-            @Override
-            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-                List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
-                Calendar calendar = Calendar.getInstance();
-                Calendar calendar1 = Calendar.getInstance();
-                calendar1.add(Calendar.HOUR, 1);
-                WeekViewEvent someEvent = new WeekViewEvent(1,"sun",calendar,calendar1);
-                events.add(someEvent);
-                return events;
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.checkBox:
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean("checkBox", checked).apply();
+            case R.id.checkBox2:
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean("checkBox2", checked).apply();
+            case R.id.checkBox3:
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean("checkBox3", checked).apply();
+            case R.id.checkBox4:
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean("checkBox4", checked).apply();
+            case R.id.checkBox5:
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean("checkBox5", checked).apply();
+            case R.id.checkBox6:
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean("checkBox6", checked).apply();
+                break;
+        }
     }
 }
